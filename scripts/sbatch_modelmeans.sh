7efc1296a65487c358226b58c2fb82e4bf96f467
Anna Jo Muhich <ajmuhich@ucdavis.edu>
2025-03-04 09:59:34 -0800

    farm debug

--- scripts/sbatch_modelmeans.sh
+++ scripts/sbatch_modelmeans.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH -D /home/ajmuhich/model_means
+#SBATCH -D /home/ajmuhich/model_means/IT/
 #SBATCH -o /home/ajmuhich/slurm-log/model_means_stdout-%j.txt
 #SBATCH -e /home/ajmuhich/slurm-log/model_means_stderr-%j.txt
 #SBATCH -J model_means
@@ -9,6 +9,8 @@
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=ajmuhich@ucdavis.edu
 
+#set -x  # Enable debugging (print each command before execution)
+
 module load R
 
 # Define input file paths
@@ -17,5 +19,5 @@ SAMPLEID_FILE=$2
 BATCH_FILE=$3
 OUTPUT_DIR=$4
 
-R CMD BATCH scripts/model_means.R "$COUNTS_FILE" "$SAMPLEID_FILE" "$BATCH_FILE" "$OUTPUT_DIR"
+Rscript ~/co-transcriptome/scripts/model_means.R "$COUNTS_FILE" "$SAMPLEID_FILE" "$BATCH_FILE" "$OUTPUT_DIR"
 
