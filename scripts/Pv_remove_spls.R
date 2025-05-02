9cc9e731f1388a52f489ffc1fbd262f8c91ae136
annajomuhich <102766766+annajomuhich@users.noreply.github.com>
2025-05-02 11:34:53 -0700

    remove bad isos

--- scripts/Pv_remove_spls.R
+++ scripts/Pv_remove_spls.R
@@ -8,12 +8,12 @@ library(edgeR)
 ######## Load data ================================
 
 #### Specify input paths - MAKE CHANGES HERE
-hostrep1_path <- "co-transcriptome/raw_reads/all_samples/UCC1/Host_readcounts.csv"
-hostrep2_path <- "co-transcriptome/raw_reads/all_samples/UCC2/Host_readcounts.csv"
-hostrep3_path <- "co-transcriptome/raw_reads/all_samples/UCC3/Host_readcounts.csv"
-bcinrep1_path <- "co-transcriptome/raw_reads/all_samples/UCC1/Bcin_readcounts.csv"
-bcinrep2_path <- "co-transcriptome/raw_reads/all_samples/UCC2/Bcin_readcounts.csv"
-bcinrep3_path <- "co-transcriptome/raw_reads/all_samples/UCC3/Bcin_readcounts.csv"
+hostrep1_path <- "raw_reads/all_samples/UCC1/Host_readcounts.csv"
+hostrep2_path <- "raw_reads/all_samples/UCC2/Host_readcounts.csv"
+hostrep3_path <- "raw_reads/all_samples/UCC3/Host_readcounts.csv"
+bcinrep1_path <- "raw_reads/all_samples/UCC1/Bcin_readcounts.csv"
+bcinrep2_path <- "raw_reads/all_samples/UCC2/Bcin_readcounts.csv"
+bcinrep3_path <- "raw_reads/all_samples/UCC3/Bcin_readcounts.csv"
 ####
 
 #read in data
@@ -28,6 +28,7 @@ bcinrep3 <- read.csv(bcinrep3_path)
 
 ### MAKE CHANGES HERE FOR YOUR DATASET BASED ON MULTIQC and PCAs
 
+#remove bad QC samples
 hostrep2 <- hostrep2 %>%
 	dplyr::select(!U11_2) %>%
 	dplyr::select(!U57_2) %>%
@@ -39,10 +40,32 @@ bcinrep2 <- bcinrep2 %>%
 	dplyr::select(!U64_2) %>%
 	dplyr::select(!U52_2)
 
+#remove low readcount isolates
+# List of samples prefixes to remove
+prefixes <- c("U21_", "U72_", "U11_", "U47_", "U44_", "U34_", "U7_")
+# Combine into a single regex pattern
+pattern <- paste0("^(", paste(prefixes, collapse = "|"), ")")
+# Function to remove columns whose names start with any of the prefixes
+remove_unwanted_cols <- function(df) {
+	df[ , !grepl(pattern, names(df))]
+}
+# Put dataframe in list
+dfs <- list(bcinrep1, bcinrep2, bcinrep3,
+						hostrep1, hostrep2, hostrep3)
+# Apply the function to each dataframe
+dfs <- lapply(dfs, remove_unwanted_cols)
+#pull dfs back out
+bcinrep1 <- dfs[[1]]
+bcinrep2 <- dfs[[2]]
+bcinrep3 <- dfs[[3]]
+hostrep1 <- dfs[[4]]
+hostrep2 <- dfs[[5]]
+hostrep3 <- dfs[[6]]
+
 ######## Save new datasets ===================================
 
-dir.create("co-transcriptome/raw_reads/badQC_removed")
-setwd("co-transcriptome/raw_reads/badQC_removed/")
+dir.create("raw_reads/badQC_removed")
+setwd("raw_reads/badQC_removed/")
 
 dir.create("UCC1")
 dir.create("UCC2")
--- scripts/Vu_remove_spls.R
+++ scripts/Vu_remove_spls.R
@@ -8,12 +8,12 @@ library(edgeR)
 ######## Load data ================================
 
 #### Specify input paths - MAKE CHANGES HERE
-hostrep1_path <- "co-transcriptome/raw_reads/all_samples/IT1/Host_readcounts.csv"
-hostrep2_path <- "co-transcriptome/raw_reads/all_samples/IT2/Host_readcounts.csv"
-hostrep3_path <- "co-transcriptome/raw_reads/all_samples/IT3/Host_readcounts.csv"
-bcinrep1_path <- "co-transcriptome/raw_reads/all_samples/IT1/Bcin_readcounts.csv"
-bcinrep2_path <- "co-transcriptome/raw_reads/all_samples/IT2/Bcin_readcounts.csv"
-bcinrep3_path <- "co-transcriptome/raw_reads/all_samples/IT3/Bcin_readcounts.csv"
+hostrep1_path <- "raw_reads/all_samples/IT1/Host_readcounts.csv"
+hostrep2_path <- "raw_reads/all_samples/IT2/Host_readcounts.csv"
+hostrep3_path <- "raw_reads/all_samples/IT3/Host_readcounts.csv"
+bcinrep1_path <- "raw_reads/all_samples/IT1/Bcin_readcounts.csv"
+bcinrep2_path <- "raw_reads/all_samples/IT2/Bcin_readcounts.csv"
+bcinrep3_path <- "raw_reads/all_samples/IT3/Bcin_readcounts.csv"
 ####
 
 #read in data
@@ -28,6 +28,7 @@ bcinrep3 <- read.csv(bcinrep3_path)
 
 ### MAKE CHANGES HERE FOR YOUR DATASET BASED ON MULTIQC and PCAs
 
+#remove bad QC samples
 #We have 2 libraries for I27_1, because yield was bad on the first library.
 #The one from IT2 worked much better and has ~10X more readcounts. Removing I27_1 from rep 1.
 hostrep1 <- hostrep1 %>% select(!I27_1)
@@ -44,10 +45,32 @@ bcinrep3 <- bcinrep3 %>%
 	dplyr::select(!I10_3) %>% #good seq count, but ton of overrepresented sequences
 	dplyr::select(!I5_4)  #good seq count, but ton of overrepresented sequences
 
+#remove low readcount isolates
+# List of samples prefixes to remove
+prefixes <- c("I21_", "I72_", "I11_", "I47_", "I44_", "I34_", "I7_")
+# Combine into a single regex pattern
+pattern <- paste0("^(", paste(prefixes, collapse = "|"), ")")
+# Function to remove columns whose names start with any of the prefixes
+remove_unwanted_cols <- function(df) {
+	df[ , !grepl(pattern, names(df))]
+}
+# Put dataframe in list
+dfs <- list(bcinrep1, bcinrep2, bcinrep3,
+						hostrep1, hostrep2, hostrep3)
+# Apply the function to each dataframe
+dfs <- lapply(dfs, remove_unwanted_cols)
+#pull dfs back out
+bcinrep1 <- dfs[[1]]
+bcinrep2 <- dfs[[2]]
+bcinrep3 <- dfs[[3]]
+hostrep1 <- dfs[[4]]
+hostrep2 <- dfs[[5]]
+hostrep3 <- dfs[[6]]
+
 ######## Save new datasets ===================================
 
-dir.create("co-transcriptome/raw_reads/badQC_removed")
-setwd("co-transcriptome/raw_reads/badQC_removed/")
+dir.create("raw_reads/badQC_removed")
+setwd("raw_reads/badQC_removed/")
 
 dir.create("IT1")
 dir.create("IT2")
