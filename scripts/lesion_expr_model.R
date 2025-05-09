fa83ea0437f504a033cb5ff2b7df60c3559cf3d9
annajomuhich <102766766+annajomuhich@users.noreply.github.com>
2025-05-09 15:30:27 -0700

    lesion expr model

---
+++ scripts/lesion_expr_model.R
@@ -0,0 +1,132 @@
+### Modeling transcript effect on lesion size
+### May 2025 AJM
+
+##### ============= Define paths ============== ######
+
+lesion_path <- "~/UCDavis/Klieb_Lab/Projects/Fabaceae/Fab_Manuscript/data/Pheno/cucfab_lsm.csv"
+bcin_expr_path <- "remove_7isos/bcin_genemodel_may2/bcin_adjusted_emmeans.csv"
+pv_expr_path <- "remove_7isos/Pv_hostmodel/adjusted_emmeans.csv"
+vu_expr_path <- "remove_7isos/Vu_hostmodel/adjusted_emmeans.csv"
+
+##### ============= Load data and libraries ===== ######
+
+library(tidyverse)
+library(lme4)
+
+lesion <- read.csv(lesion_path)
+bcin_expr <- read.csv(bcin_expr_path)
+pv_expr <- read.csv(pv_expr_path)
+vu_expr <- read.csv(vu_expr_path)
+
+##### ============= Reformat data - Bcin model ================
+
+lesion <- lesion %>% select(Isolate_name, UCC, IT)
+lesion <- lesion %>% pivot_longer(cols = !Isolate_name,
+																	names_to = "genotype",
+																	values_to = "lesion_size")
+lesion <- lesion %>% rename(iso_name = Isolate_name)
+
+
+
+expr <- left_join(bcin_expr, pv_expr, by = c("iso_name", "genotype"))
+expr <- left_
+
+df <- left_join(bcin_expr, lesion, by = c("iso_name", "genotype")) %>%
+	mutate(host = if_else(genotype == "UCC", "common bean", "cowpea")) %>%
+	select(iso_name, host, lesion_size, everything()) %>%
+	select(!genotype) %>%
+	filter(iso_name != "Mock_48HAI")
+
+df$host <- as.factor(df$host)
+
+##### ============== Loop - Bcin model =================
+
+#get list of transcripts to loop through
+genes <- colnames(df)[grep("^Bcin", colnames(df))]
+
+#set up dataframe with first gene in the list
+gene <- genes[1]
+
+message(paste("modeling", gene))
+
+#Note: this one wasn't working, I think because you can't assign continuous variables as random effects
+#formula <- as.formula(lesion_size ~ (1|Bcin01g00010) + host + (Bcin01g00010 * host))
+
+formula <- as.formula(paste("lesion_size ~", gene, "* host"))
+
+model <- lm(formula, data = df) #works
+#extract anova table
+anova <- print(car::Anova(model)) #save to object. this also displays on output
+anova <- rownames_to_column(anova, var = "variable") #get column for variable category
+
+#gather variance data
+fixed_var <- (diag(vcov(model)))
+# fixed_var is a named numeric vector from diag(vcov(model))
+percent_var <- 100 * fixed_var / sum(fixed_var)
+# Put into a data frame for clarity
+fixed_var_df <- data.frame(
+	term = names(fixed_var),
+	variance = percent_var)
+fixed_var_df <- fixed_var_df %>%
+	mutate(variable = gsub("hostcowpea", "host", term)) %>%
+	select(!term)
+
+#join to anova data
+anova <- full_join(anova, fixed_var_df, by = "variable")
+anova <- anova %>%
+	mutate(variable = gsub(paste(gene), "gene_expression", variable))
+anova$gene <- gene
+anova <- anova %>% select(gene, everything())
+
+anova_all <- anova #initialize df for combined anovas
+
+#define list for failed genes
+failed_genes <- list()
+
+#adjust gene list
+genes <- genes[-1]
+
+for (gene in genes) {
+	message(paste("modeling", gene))
+	tryCatch({
+		formula <- as.formula(paste("lesion_size ~", gene, "* host"))
+		
+		model <- lm(formula, data = df)
+		
+		# extract ANOVA table
+		anova <- car::Anova(model)  # don't wrap in print(), this returns the object
+		anova <- tibble::rownames_to_column(as.data.frame(anova), var = "variable")
+		
+		# gather variance data
+		fixed_var <- diag(vcov(model))
+		percent_var <- 100 * fixed_var / sum(fixed_var)
+		fixed_var_df <- data.frame(
+			term = names(fixed_var),
+			variance = percent_var
+		) %>%
+			mutate(variable = gsub("hostcowpea", "host", term)) %>%
+			select(-term)
+		
+		# join to ANOVA data
+		anova <- dplyr::full_join(anova, fixed_var_df, by = "variable") %>%
+			mutate(variable = gsub(gene, "gene_expression", variable))
+		
+		anova$gene <- gene
+		anova <- dplyr::select(anova, gene, everything())
+		
+		# append to growing ANOVA dataframe
+		anova_all <- rbind(anova_all, anova)
+		
+	}, error = function(e) {
+		message(paste("Error encountered for gene:", gene, "Skipping..."))
+		failed_genes <<- c(failed_genes, gene)
+	})
+}
+
+##### ================ Reformat data - Host ==========================
+
+pv_expr <- pv_expr %>% select(-infected) %>% filter(iso_name != "Mock")
+pv_expr$genotype <- "UCC"
+vu_expr <- vu_expr %>% select(-infected) %>% filter(iso_name != "Mock")
+vu_expr$genotype <- "IT"
+
